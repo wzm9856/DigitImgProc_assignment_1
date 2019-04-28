659541dca9ffef97953cff14f754647597adc170
wzm9856 <wzm9856@gmail.com>
2019-04-28 15:36:18 +0800

    fix global equalization

--- Source.cpp
+++ Source.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 using namespace cv;
-void getStatData(Mat src, int* hist, float* ave, float* var, bool isPrint) {
+void getStatData(Mat src, int* hist, float* ave, float* var) {
 	float average = 0, variance = 0;
 	int channels = src.channels();
 	int nRows = src.rows;
@@ -24,52 +24,57 @@ void getStatData(Mat src, int* hist, float* ave, float* var, bool isPrint) {
 			hist[p[j]]++;
 		}
 	}
-	int max = *max_element(hist, hist + 256);
-	Mat histgraph = Mat::zeros(300, 512, CV_8U);
-	for (i = 0; i < 256; ++i) {
-		if (isPrint) {
-			for (j = histgraph.rows - ((double)hist[i] / max * (histgraph.rows - 1)); j < histgraph.rows; ++j) {
-				histgraph.at<uchar>(j, 2 * i) = 255;
-				histgraph.at<uchar>(j, 2 * i + 1) = 255;
-			}
-		}
+	for (i = 0; i < 256; i++) {
 		average += i * hist[i];
 	}
-
 	average /= src.rows * src.cols;
 	for (i = 0; i < 256; ++i) {
 		variance += hist[i] * (i - average) * (i - average);
 	}
 	variance /= src.rows * src.cols;
 	*ave = average; *var = variance;
-	if (isPrint) {
-		imshow("Original Image", src);
-		imshow("Histgraph", histgraph);
-		cout << "图片的全局平均值为" << average << "，方差为" << variance << endl;
-	}
+	cout << "图片的全局平均值为" << average << "，方差为" << variance << endl;
 }
 
-Mat getlut(int* hist) {
-	//积分直方图
-	int addArray[256] = { 0 };
-	addArray[0] = hist[0];
-	for (int i = 1; i < 256; i++) {
-		addArray[i] = addArray[i - 1] + hist[i];
+void printHist(Mat src, int* hist){
+	int max = *max_element(hist, hist + 256);
+	Mat histgraph = Mat::zeros(300, 512, CV_8U);
+	for (int i = 0; i < 256; ++i) {
+		for (int j = histgraph.rows - ((double)hist[i] / max * (histgraph.rows - 1)); j < histgraph.rows; ++j) {
+			histgraph.at<uchar>(j, 2 * i) = 255;
+			histgraph.at<uchar>(j, 2 * i + 1) = 255;
+		}
 	}
+	imshow("Original Image", src);
+	imshow("Histgraph", histgraph);
+}
+
+void interHist(int* hist, int* addedHist) {
+	//积分直方图
+	addedHist[0] = hist[0];
+	for (int i = 1; i < 256; i++) {
+		addedHist[i] = addedHist[i - 1] + hist[i];
+	}
+}
+Mat getlut(int* addedHist) {
 	//转换到调色盘
 	Mat lut(1, 256, CV_8UC1);
 	uchar* p = lut.ptr();
 	for (int i = 0; i < 256; i++) {
-		p[i] = addArray[i] / addArray[255] * 255;
-
+		p[i] = 255.0 * addedHist[i] / addedHist[255];
+		cout << "addedHist" << i << ":" << addedHist[i] << endl;
+		cout << "p" << i << ": " << int(p[i]) << endl;
 	}
 	return lut;
 }
 
 Mat equalization(Mat src) {
-	float average, variance; int hist[256], addArray[256];
-	getStatData(src, hist, &average, &variance, 0);
-	Mat newLookUpTable = getlut(hist);
+	//基础的全局直方图均衡化程序
+	float average, variance; int hist[256] = { 0 }, addedHist[256] = { 0 };
+	getStatData(src, hist, &average, &variance);
+	//printHist(src, hist);
+	interHist(hist, addedHist);
+	Mat newLookUpTable = getlut(addedHist);
 	Mat res(src.rows, src.cols, src.type());
 	LUT(src, newLookUpTable, res);
 	return res;
@@ -83,7 +88,8 @@ int main()
 	float average, variance;
 	double t = (double)getTickCount();
 	int hist[256] = { 0 };
-	getStatData(image, hist, &average, &variance, 1);
+	getStatData(image, hist, &average, &variance);
+	printHist(image, hist);
 	t = ((double)getTickCount() - t) / getTickFrequency();
 	cout << "此段程序用时为" << t << "s" << endl;
 	waitKey(0);
